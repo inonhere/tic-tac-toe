aaba1e31f8e4d5c634be8abe85c37a5a090c377a
Inon <pokemonxdloquendo@gmail.com>
2022-07-14 16:53:59 -0400

    all three rows, separators and display of board

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
 class GameBoard
+    attr_accessor :block_1
+
     def initialize
         @block_1 = '.'
         @block_2 = '.'
@@ -13,9 +15,23 @@ class GameBoard
     end
 
     def row_1
-       puts @block_1 + @separator + @block_2
+       @block_1 + @separator + @block_2 + @separator + @block_3
+    end
+
+    def row_2
+        @block_4 + @separator + @block_5 + @separator + @block_6
+    end
+
+    def row_3
+        @block_7 + @separator + @block_8 + @separator + @block_9
+    end
+
+    def board
+        puts row_1
+        puts row_2
+        puts row_3
     end
 end
 
 gameboard = GameBoard.new
-gameboard.row_1
+gameboard.board
