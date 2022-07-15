c902b44663775151dc9607ac99fe4c4122625b0b
Inon <pokemonxdloquendo@gmail.com>
2022-07-15 10:15:14 -0400

    player turns, change of two points

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 class GameBoard
-    attr_accessor :block_1
+
+    attr_writer :block_1, :block_2, :block_3, :block_4, :block_5, :block_6, :block_7, :block_8, :block_9
 
     def initialize
         @block_1 = '.'
@@ -32,6 +33,38 @@ class GameBoard
         puts row_3
     end
 end
+class Game < GameBoard
+    gameboard = GameBoard.new
 
-gameboard = GameBoard.new
-gameboard.board
+    @@player_1 = true
+    @@player_2 = false
+    while @@player_1 == true
+        gameboard.board
+        puts "player 1 turn"
+        x = gets.chomp
+        if x == "1"
+            gameboard.block_1=('X')
+            gameboard.board
+            @@player_1 = false
+            @@player_2 = true
+        elsif x == "2"
+            gameboard.block_2=('X')
+            gameboard.board
+            @@player_1 = false
+            @@player_2 = true
+        end
+    end
+    while @@player_2 == true
+        gameboard.board
+        puts "player 2 turn"
+        o = gets.chomp
+        if o == '2'
+            gameboard.block_2=('O')
+            gameboard.board
+            @@player_2 = false
+            @@player_1 = true
+        end
+    end
+
+end
+game = Game::new
