ca9f7e93ed07bdda00de7c9e89d7e0f0d4c386de
Inon <pokemonxdloquendo@gmail.com>
2022-07-15 12:00:08 -0400

    win conditions for both players

--- main.rb
+++ main.rb
@@ -40,6 +40,10 @@ class Game < GameBoard
     @@player_2 = false
     @@winner = false
     
+    puts "To select where to put your movement input a number from 1 to 9"
+    puts "First row it's 1 2 3"
+    puts "Second row it's 4 5 6"
+    puts "Third row its 7 8 9 "
     while @@winner == false
         while @@player_1 == true and @@winner == false
             gameboard.board
@@ -65,13 +69,63 @@ class Game < GameBoard
                 gameboard.board
                 @@player_1 = false
                 @@player_2 = true
+            elsif x == '5' and gameboard.block_5 == '.'
+                gameboard.block_5=('X')
+                @@player_1 = false
+                @@player_2 = true
+            elsif x == '6' and gameboard.block_6 == '.'
+                gameboard.block_6=('X')
+                @@player_1 = false
+                @@player_2 = true
+            elsif x == '7' and gameboard.block_7 == '.'
+                gameboard.block_7=('X')
+                @@player_1 = false
+                @@player_2 = true
+            elsif x == '8' and gameboard.block_8 == '.'
+                gameboard.block_8=('X')
+                @@player_1 = false
+                @@player_2 = true
+            elsif x == '9' and gameboard.block_9 == '.'
+                gameboard.block_9=('X')
+                @@player_1 = false
+                @@player_2 = true
             end
+
             if gameboard.block_1 == 'X' and gameboard.block_2 == 'X' and gameboard.block_3 == 'X'
-                puts "Player 1 is the winner"
+                puts "Player 1 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_4 == 'X' and gameboard.block_5 == 'X' and gameboard.block_6 == 'X'
+                puts "Player 1 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_7 == 'X' and gameboard.block_8 == 'X' and gameboard.block_9 == 'X'
+                puts "Player 1 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_1 == 'X' and gameboard.block_4 == 'X' and gameboard.block_7 == 'X'
+                puts "Player 1 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_2 == 'X' and gameboard.block_5 == 'X' and gameboard.block_8 == 'X'
+                puts "Player 1 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_3 == 'X' and gameboard.block_6 == 'X' and gameboard.block_9 == 'X'
+                puts "Player 1 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_1 == 'X' and gameboard.block_5 == 'X' and gameboard.block_9 == 'X'
+                puts "Player 1 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_3 == 'X' and gameboard.block_5 == 'X' and gameboard.block_7 == 'X'
+                puts "Player 1 wins"
+                gameboard.board
                 @@winner = true
             end
         end
-    
+        
         while @@player_2 == true and @@winner == false
             gameboard.board
             puts "player 2 turn"
@@ -101,12 +155,62 @@ class Game < GameBoard
                 gameboard.board
                 @@player_2 = false
                 @@player_1 = true
+            elsif o == '6' and gameboard.block_6 == '.'
+                gameboard.block_6=('O')
+                gameboard.board
+                @@player_2 = false
+                @@player_1 = true
+            elsif o == '7' and gameboard.block_7 == '.'
+                gameboard.block_7=('O')
+                gameboard.board
+                @@player_2 = false
+                @@player_1 = true
+            elsif o == '8' and gameboard.block_8 == '.'
+                gameboard.block_8=('O')
+                gameboard.board
+                @@player_2 = false
+                @@player_1 = true
+            elsif o == '9' and gameboard.block_9 == '.'
+                gameboard.block_9=('O')
+                gameboard.board
+                @@player_2 = false
+                @@player_1 = true
+            end
+            
+            if gameboard.block_1 == 'O' and gameboard.block_2 == 'O' and gameboard.block_3 == 'O'
+                puts "Player 2 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_4 == 'O' and gameboard.block_5 == 'O' and gameboard.block_6 == 'O'
+                puts "Player 2 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_7 == 'O' and gameboard.block_8 == 'O' and gameboard.block_9 == 'O'
+                puts "Player 2 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_1 == 'O' and gameboard.block_4 == 'O' and gameboard.block_7 == 'O'
+                puts "Player 2 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_2 == 'O' and gameboard.block_5 == 'O' and gameboard.block_8 == 'O'
+                puts "Player 2 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_3 == 'O' and gameboard.block_6 == 'O' and gameboard.block_9 == 'O'
+                puts "Player 2 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_1 == 'O' and gameboard.block_5 == 'O' and gameboard.block_9 == 'O'
+                puts "Player 2 wins"
+                gameboard.board
+                @@winner = true
+            elsif gameboard.block_3 == 'O' and gameboard.block_5 == 'O' and gameboard.block_7 == 'O'
+                puts "Player 2 wins"
+                gameboard.board
+                @@winner = true
             end
         end
-        
-
-
-        puts gameboard.block_1
     end
 end
 game = Game::new
