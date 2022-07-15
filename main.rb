c084dca25d08606630ebf9bb1af279ab9fc97694
Inon <pokemonxdloquendo@gmail.com>
2022-07-15 10:27:29 -0400

    gameloop and change of turns between players

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 class GameBoard
 
-    attr_writer :block_1, :block_2, :block_3, :block_4, :block_5, :block_6, :block_7, :block_8, :block_9
+    attr_accessor :block_1, :block_2, :block_3, :block_4, :block_5, :block_6, :block_7, :block_8, :block_9
 
     def initialize
         @block_1 = '.'
@@ -38,33 +38,62 @@ class Game < GameBoard
 
     @@player_1 = true
     @@player_2 = false
-    while @@player_1 == true
-        gameboard.board
-        puts "player 1 turn"
-        x = gets.chomp
-        if x == "1"
-            gameboard.block_1=('X')
+    @@winner = false
+    
+    while @@winner == false
+        while @@player_1 == true
             gameboard.board
-            @@player_1 = false
-            @@player_2 = true
-        elsif x == "2"
-            gameboard.block_2=('X')
+            puts "player 1 turn"
+            x = gets.chomp
+            if x == "1" and gameboard.block_1 == '.'
+                gameboard.block_1=('X')
+                gameboard.board
+                @@player_1 = false
+                @@player_2 = true
+            elsif x == "2" and gameboard.block_2 == '.'
+                gameboard.block_2=('X')
+                gameboard.board
+                @@player_1 = false
+                @@player_2 = true
+            elsif x == '3' and gameboard.block_3 == '.'
+                gameboard.block_3=('X')
+                gameboard.board
+                @@player_1 = false
+                @@player_2 = true
+            elsif x == '4' and gameboard.block_4 == '.'
+                gameboard.block_4=('X')
+                gameboard.board
+                @@player_1 = false
+                @@player_2 = true
+            end
+        end
+    
+        while @@player_2 == true
             gameboard.board
-            @@player_1 = false
-            @@player_2 = true
+            puts "player 2 turn"
+            o = gets.chomp
+            if o == '1' and gameboard.block_1 == '.'
+                gameboard.block_1=('O')
+                gameboard.board
+                @@player_2 = false
+                @@player_1 = true
+            elsif o == '2' and gameboard.block_2 == '.'
+                gameboard.block_2=('O')
+                gameboard.board
+                @@player_2 = false
+                @@player_1 = true
+            elsif o == '3' and gameboard.block_3 == '.'
+                @gameboard.block_3=('O')
+                gameboard.board
+                @@player_2 = false
+                @@player_1 = true
+            elsif o == '4' and gameboard.block_4 == '.'
+                gameboard.block_4=('O')
+                gameboard.board
+                @@player_2 = false
+                @@player_1 = true
+            end
         end
     end
-    while @@player_2 == true
-        gameboard.board
-        puts "player 2 turn"
-        o = gets.chomp
-        if o == '2'
-            gameboard.block_2=('O')
-            gameboard.board
-            @@player_2 = false
-            @@player_1 = true
-        end
-    end
-
 end
 game = Game::new
